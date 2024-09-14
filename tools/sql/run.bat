66a810775aed50192b84f5cbe3e85d9e04d7adc8
Rufus Zero <rufus.nero.m@gmail.com>
2024-09-14 06:35:59 +0300

    chore: update default mariadb params
    Change default MariaDB volume name to onyxdb-files
    
    Update dockerfile to set a 24 hours wait_timeout configuration for MariaDB. This fixes the problem where server idles overnight and by the morning the database has dropped the idle connection. All the connecting players then observe their data missing.
    
    Added a comment about importance of the proper password (although check all things security, don't rely on just password) for production servers. In just 2 days the server and database were up for a test launch the database got brute-forced, all the tables dumped and dropped, and a note left in a new table demanding $800 in bitcoin for recovery of the data (a single row with permissions for my testing, lol).

--- tools/sql/run.bat
+++ tools/sql/run.bat
@@ -1,7 +1,7 @@
 @echo off
 docker start onyxdb 2>nul
 if errorlevel 1 (
-	docker run -d -v zero-onyx-db-files:/var/lib/mysql --network host --name onyxdb onyxdb --default_authentication_plugin=mysql_native_password
+	docker run -d -v onyxdb-files:/var/lib/mysql --network host --name onyxdb onyxdb --default_authentication_plugin=mysql_native_password
 	if errorlevel 1 (
 		echo.
 		echo Failed to start the container!
--- tools/sql/run.sh
+++ tools/sql/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 docker start onyxdb > /dev/null 2>&1 || {
-	docker run -d -v zero-onyx-db-files:/var/lib/mysql --network host --name onyxdb onyxdb --default_authentication_plugin=mysql_native_password
+	docker run -d -v onyxdb-files:/var/lib/mysql --network host --name onyxdb onyxdb --default_authentication_plugin=mysql_native_password
 }
 echo
 echo "Container started!"
